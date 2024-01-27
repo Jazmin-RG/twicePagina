59945b14201404b0125a21ef82de2a472509a070
Jazmin-RG <rodriguezgonzalezjazmin005@gmail.com>
2024-01-26 21:43:04 -0600

    fondo e icono

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ for(let i=0; i<=34; i++){
     <div class="card">
         <div class="color">
         </div>
-        <button>+</button>
+        <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#FF5FA2" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
         <p>Primer canción que escuchaste</p>
     </div>
     `;
