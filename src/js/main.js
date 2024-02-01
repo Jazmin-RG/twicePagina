4dd49068de26b14e0b96c619de3ec4df18eea871
Jazmin-RG <rodriguezgonzalezjazmin005@gmail.com>
2024-01-31 22:53:18 -0600

    agregar eventos

--- src/js/main.js
+++ src/js/main.js
@@ -113,19 +113,28 @@ let datos=[{
 ]
 
 
-for(let i=0; i<4; i++){
+/*for(let i=0; i<4; i++){
     console.log(datos[i]);
     console.log(datos[i].pregunta);
     console.log(datos[i].color);
-}
+}*/
 
 for(let i=0; i<datos.length; i++){
     cards.innerHTML += `
     <div class="card">
         <div class="espacio ${datos[i].color}">
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#FF5FA2" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+        <img class="icon" id=${datos[i].pregunta} src="./src/img/button.png" alt="">
         <p>${datos[i].pregunta}</p>
     </div>
     `;
 }; 
+
+const btnAddInfo=document.querySelectorAll('.icon');
+console.log(btnAddInfo);
+ 
+btnAddInfo.forEach((btnInfo) =>{
+    btnInfo.addEventListener("click",(e)=>{
+        console.log("Te amo mucho osito, dame " + e.target.id + " besito");
+    });
+});
